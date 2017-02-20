4c8fa56eb46b73435815aae0a3e1eccee7475acd
Nesmark <yarikvfx@gmail.com>
2017-02-20 12:24:32 +0200

    'videoPlayer'

---
+++ 11day/scripts-FINISHED.js
@@ -0,0 +1,69 @@
+
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const playerBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const loop = player.querySelector('.loop');
+const full = player.querySelector('.full');
+////////////////////////////////////////////////////////////////////////////
+
+function togglePlay(){
+ const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+function updateButton(){
+ const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+function skip(){
+ video.currentTime += parseFloat(this.dataset.skip)
+}
+function looping(v){
+ if(v){
+  console.log(v)
+  video.loop = true;
+  loop.textContent = 'looping';
+   
+ }else{
+  video.loop = false;
+  loop.textContent = 'loop';
+ }
+}
+function handleRangeUpdate(){
+  video[this.name] = this.value;
+}
+function handleProgress(){
+  const percent = (video.currentTime / video.duration)* 100;
+  playerBar.style.flexBasis = `${percent}%`
+}
+function scrub(e){
+ const scrubTime = (e.offsetX / progress.offsetWidth)* video.duration;
+  video.currentTime = scrubTime;
+}
+////////////////////////////////////////////////////////////////////////////
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(v=>v.addEventListener('click',skip));
+
+let clickloop = false;
+loop.addEventListener('click',()=>{clickloop = !clickloop; looping(clickloop)});
+full.addEventListener('click', () => video.webkitRequestFullscreen())
+
+ranges.forEach((v)=>{
+              v.addEventListener('change',handleRangeUpdate);
+              v.addEventListener('mousemove',handleRangeUpdate);
+                    });
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown',() => mousedown = true);
+progress.addEventListener('mouseup',() => mousedown = false);
